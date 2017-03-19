efc9dd1dbe8ebbb9371c0a78c0d85ab3a7413a48
David Thole <dthole@gmail.com>
2017-03-19 17:16:34 -0500

    Changing to referece my install for docker

--- launchdocker.sh
+++ launchdocker.sh
@@ -2,8 +2,8 @@
 
 # Cleanup old images, if they exist
 docker rm mssql
-docker rm rstudioweb
 docker rm shinyserver
+docker rm rstudioweb
 
 # Launch SQL Server
 docker run -v '/Users/dthole/Programming/r-shiny-docker:/data' -e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD=pAssw04d' --name mssql -p 1433:1433 -d microsoft/mssql-server-linux
@@ -12,7 +12,7 @@ docker run -v '/Users/dthole/Programming/r-shiny-docker:/data' -e 'ACCEPT_EULA=Y
 docker run -d --name shinyserver -p 3838:3838 --link mssql:mssql -v '/Users/dthole/Programming/r-shiny-docker/Shiny:/srv/shiny-server/' -v '/Users/dthole/Programming/r-shiny-docker/Shiny.Logs:/var/log/shiny-server/' rocker/shiny
 
 # Launch rstudioweb
-docker run -v '/Users/dthole/Programming/r-shiny-docker:/home/rstudio/src' --name rstudioweb --link mssql:mssql -d -p 8787:8787 rocker/rstudio
+docker run -v '/Users/dthole/Programming/r-shiny-docker:/home/rstudio/src' --name rstudioweb --link mssql:mssql -d -p 8787:8787 dthole/shiny:v1
 
 echo "Rstudio Server: http://localhost:8787"
 echo "Shiny Server: http://localhost:3838"
