9020c1fadb015e2469c2cce26f34621c6f580d56
David Thole <dthole@gmail.com>
2017-04-08 15:26:01 -0500

    Updating the launch script

--- launchdocker.sh
+++ launchdocker.sh
@@ -9,10 +9,10 @@ docker rm rstudioweb
 docker run -v '/Users/dthole/Programming/r-shiny-docker:/data' -e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD=pAssw04d' --name mssql -p 1433:1433 -d microsoft/mssql-server-linux
 
 # Launch Shiny Server
-docker run -d --name shinyserver -p 3838:3838 --link mssql:mssql -v '/Users/dthole/Programming/r-shiny-docker/Shiny:/srv/shiny-server/' -v '/Users/dthole/Programming/r-shiny-docker/Shiny.Logs:/var/log/shiny-server/' rocker/shiny
+docker run -d --name shinyserver -p 3838:3838 --link mssql:mssql -v '/Users/dthole/Programming/r-shiny-docker/Shiny:/srv/shiny-server/' -v '/Users/dthole/Programming/r-shiny-docker/Shiny.Logs:/var/log/shiny-server/' dthole/shiny:v1
 
 # Launch rstudioweb
-docker run -v '/Users/dthole/Programming/r-shiny-docker:/home/rstudio/src' --name rstudioweb --link mssql:mssql -d -p 8787:8787 dthole/rstudio:v1
+docker run -v '/Users/dthole/Programming/r-shiny-docker:/home/rstudio/src' --name rstudioweb --link mssql:mssql -d -p 8787:8787 dthole/rstudio:v2
 
 echo "Rstudio Server: http://localhost:8787"
 echo "Shiny Server: http://localhost:3838"
