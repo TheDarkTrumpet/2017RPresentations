cbba99cfe73c89e6a049e5677b168c01ad074557
David Thole <dthole@gmail.com>
2017-04-10 16:30:32 -0500

    Adding a restore database, that's run through sqlcmd

--- launchdocker.sh
+++ launchdocker.sh
@@ -8,14 +8,17 @@ docker rm rstudioweb
 # Launch SQL Server
 docker run -v '/Users/dthole/Programming/r-shiny-docker:/data' -e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD=pAssw04d' --name mssql -p 1433:1433 -d microsoft/mssql-server-linux
 
-# Restore database backup
-docker run -v '/Users/dthole/Programming/r-shiny-docker/:/data/' --link mssql:mssql -it fabiang/sqlcmd -S mssql -U sa -P pAssw04d '-i/data/Data/RestoreDatabase.sql'
-
 # Launch Shiny Server
-docker run -d --name shinyserver -p 3838:3838 --link mssql:mssql -v '/Users/dthole/Programming/r-shiny-docker/Shiny:/srv/shiny-server/' -v '/Users/dthole/Programming/r-shiny-docker/Shiny.Logs:/var/log/shiny-server/' dthole/shiny:v1
+docker run -d --name shinyserver -p 3838:3838 --link mssql:mssql -v '/Users/dthole/Programming/r-shiny-docker/Shiny:/srv/shiny-server/' -v '/Users/dthole/Programming/r-shiny-docker/Shiny.Logs:/var/log/shiny-server/' thedarktrumpet/shiny:v1
 
 # Launch rstudioweb
-docker run -v '/Users/dthole/Programming/r-shiny-docker:/home/rstudio/src' --name rstudioweb --link mssql:mssql -d -p 8787:8787 dthole/rstudio:v2
+docker run -v '/Users/dthole/Programming/r-shiny-docker:/home/rstudio/src' --name rstudioweb --link mssql:mssql -d -p 8787:8787 thedarktrumpet/rstudio:v2
 
 echo "Rstudio Server: http://localhost:8787"
 echo "Shiny Server: http://localhost:3838"
+
+echo "Restoring database backup"
+sleep 10
+
+# Restore database backup
+docker run -v '/Users/dthole/Programming/r-shiny-docker/:/data/' --link mssql:mssql -it fabiang/sqlcmd -S mssql -U sa -P pAssw04d '-i/data/Data/RestoreDatabase.sql'
