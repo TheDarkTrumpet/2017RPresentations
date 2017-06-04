ba8631d56a35ce2f16881f382faecaf2083ea2ab
David Thole <dthole@gmail.com>
2017-06-04 15:27:24 -0500

    Adding one more level of values for the array, and adding summary

--- Src/1-RIntro/01 - Basic Operations.R
+++ Src/1-RIntro/01 - Basic Operations.R
@@ -21,7 +21,7 @@ xPlus5 <- function(x) { x + 5 }
 xPlus5(5)
 
 # Create a data structure
-x <- c("Foo", "Bar", 1, 2, 3, 4, 5, 6)
+x <- c("Foo", "Bar", 1, 2, 3, 4, 5, 6, 1, 2)
 x[2]
 xl <- as.list(x)
 xm <- matrix(x, ncol=2)
@@ -37,7 +37,10 @@ xm2[-1,]
 
 d <- as.data.frame(xm2[-1,])
 names(d) <- xm2[1,]
-rownames(d) <- c("v1", "v2", "v3")
+rownames(d) <- c("v1", "v2", "v3", "v4")
 
 d$Foo[c(1,2)]
 d[-2,]
+
+# Note the 1, 2 and the numbers beside them.
+summary(d)
